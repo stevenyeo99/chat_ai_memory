2d483db6aa3a771c8abb844271d766520c7ad6e8
steven.yeo <steven.yeo@dynrtech.com>
2025-08-08 13:51:55 +0700

    test internal index with random vector.

--- backend/src/tests/pinecone-vectordb.js
+++ backend/src/tests/pinecone-vectordb.js
@@ -9,7 +9,7 @@ const pc = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
 });
 
-const index = pc.index("speedbots");
+const index = pc.index("internal");
 
 async function embedQuery(text) {
     const model = 'multilingual-e5-large'; // or list available models via pc.inference.listModels()
@@ -20,7 +20,8 @@ async function embedQuery(text) {
 }
 
 async function queryDB() {
-    const vector = await embedQuery('how do you know FM clinic?');
+    // const vector = await embedQuery('how do you know FM clinic?');
+const vector = Array.from({ length: 1536 }, () => Math.random() * 2 - 1);
     console.log(vector.length);
     const results = await index.namespace('Documents').query({
         vector,
